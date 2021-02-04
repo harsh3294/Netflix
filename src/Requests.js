66352eca1a51c8d56027d6a2bda7401487d6708c
harsh3294 <harshc3294@gmail.com>
2021-02-04 23:03:11 +0530

    front end complete

---
+++ src/Requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "dd267cbaf5460f28e7ed2f9b52947fea";
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_generes=99`,
+};
+// https://api.themoviedb.org/3/trending/all/week?api_key=dd267cbaf5460f28e7ed2f9b52947fea&language=en-US
+export default requests;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyB9ArK-hLQpQv3PAcQ1DyZQSCjSYnbOXts",
+  authDomain: "netflix-8ea63.firebaseapp.com",
+  projectId: "netflix-8ea63",
+  storageBucket: "netflix-8ea63.appspot.com",
+  messagingSenderId: "724157594198",
+  appId: "1:724157594198:web:8918795e2861158810d9f7",
+  measurementId: "G-L4VZXQ0P3V",
+};
